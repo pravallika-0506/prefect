ba3edf656d5ef5046bf9c7cb9b16f5771b3b7cbd
pravallika-0506 <111057227+pravallika-0506@users.noreply.github.com>
2024-06-12 20:38:36 +0530

    Update operator_test.py

--- code/operator_test.py
+++ code/operator_test.py
@@ -5,6 +5,3 @@ from operators.test import YeeduOperator
 def parse_job_url():
     operator = YeeduOperator('job_url','secureconnection','loginpassword')
     operator.execute()
-
-if __name__ == "__main__":
-    parse_job_url()
