a625d8dff1281337b3fa509307a2f21252963069
pravallika-0506 <111057227+pravallika-0506@users.noreply.github.com>
2024-06-12 19:53:17 +0530

    Add files via upload

---
+++ yeedu_operator.py
@@ -0,0 +1,7 @@
+from prefect import flow, task
+from operators.test import YeeduOperator
+
+@flow(retries=0, retry_delay_seconds=5, log_prints=True)
+def parse_job_url():
+    operator = YeeduOperator('job_url','secureconnection','loginpassword')
+    operator.execute()
