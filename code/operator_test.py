39ee7fffb4fc766f503adcd7d250e5e4ad31ed14
pravallika-0506 <111057227+pravallika-0506@users.noreply.github.com>
2024-06-13 20:42:54 +0530

    Update operator_test.py

--- code/operator_test.py
+++ code/operator_test.py
@@ -1,3 +1,7 @@
+from prefect import flow, task
+from operators.yeedu import YeeduOperator
+
+
 @flow(retries=0, retry_delay_seconds=5, log_prints=True)
 def parse_job_url():
     operator = YeeduOperator(job_url='https://qa-gcp.yeedu.io/tenant/bf34353f-0df2-4015-a3fc-85ac437f967e/workspace/2/spark/conf/4/metrics?type=spark_job&page=1&limit=20',
