3b4a3bc3b32a417c1488a22ab400c61650dffced
pravallika-0506 <111057227+pravallika-0506@users.noreply.github.com>
2024-06-14 16:17:22 +0530

    Update 30_hours_job.py

--- code/30_hours_job.py
+++ code/30_hours_job.py
@@ -6,6 +6,6 @@ from operators.yeedu import YeeduOperator
 @flow(retries=0, retry_delay_seconds=5, log_prints=True)
 def job_run_30_hours():
     operator = YeeduOperator(job_url='https://qa-aws-002.yeedu.io/tenant/a6a9c5ea-57b6-4a1c-aa99-84645f675b62/workspace/74/spark/conf/890813/metrics',
-                             connection_name='secureconnection',
+                             connection_name='login',
                              login_password='loginpassword')
     operator.execute()
