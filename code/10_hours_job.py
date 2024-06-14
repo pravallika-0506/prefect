ce71c8002613997e087fd2fa5d74faf21e10cc19
pravallika-0506 <111057227+pravallika-0506@users.noreply.github.com>
2024-06-14 13:54:45 +0530

    Create 10_hours_job.py

---
+++ code/10_hours_job.py
@@ -0,0 +1,17 @@
+from prefect import flow, task
+from prefect.runner.storage import GitRepository
+from prefect_github import GitHubCredentials
+from prefect.blocks.system import Secret
+from operators.yeedu import YeeduOperator
+
+
+@flow(retries=1, retry_delay_seconds=5, log_prints=True)
+def parse_job_url():
+    operator = YeeduOperator(job_url='https://qa-gcp.yeedu.io/tenant/bf34353f-0df2-4015-a3fc-85ac437f967e/workspace/2/spark/conf/5/metrics?type=spark_job&page=1&limit=20',
+                             connection_name='secureconnection',
+                             login_password='loginpassword')
+    operator.execute()
+
+
+
+
