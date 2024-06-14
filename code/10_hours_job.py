0449208d5a3fc1b799a0dded26f19e413a4bad26
pravallika-0506 <111057227+pravallika-0506@users.noreply.github.com>
2024-06-14 14:13:48 +0530

    Update 10_hours_job.py

--- code/10_hours_job.py
+++ code/10_hours_job.py
@@ -7,7 +7,7 @@ from operators.yeedu import YeeduOperator
 
 @flow(retries=1, retry_delay_seconds=5, log_prints=True)
 def yeedu_job_run():
-    operator = YeeduOperator(job_url='https://qa-gcp.yeedu.io/tenant/bf34353f-0df2-4015-a3fc-85ac437f967e/workspace/2/spark/conf/5/metrics?type=spark_job&page=1&limit=20',
+    operator = YeeduOperator(job_url='https://qa-aws-002.yeedu.io/tenant/a6a9c5ea-57b6-4a1c-aa99-84645f675b62/workspace/20/spark/conf/890808/metrics?type=spark_job&page=1&limit=20',
                              connection_name='secureconnection',
                              login_password='loginpassword')
     operator.execute()
