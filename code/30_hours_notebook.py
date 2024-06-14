0c8294d268e8d39fd817c9f9fa609a16ff4afa12
pravallika-0506 <111057227+pravallika-0506@users.noreply.github.com>
2024-06-14 16:59:31 +0530

    Create 30_hours_notebook.py

---
+++ code/30_hours_notebook.py
@@ -0,0 +1,11 @@
+from prefect import flow, task
+from prefect.blocks.system import Secret
+from operators.yeedu import YeeduOperator
+
+
+@flow(retries=0, retry_delay_seconds=5, log_prints=True)
+def notebook_run_30_hours():
+    operator = YeeduOperator(job_url='https://qa-aws-002.yeedu.io/tenant/a6a9c5ea-57b6-4a1c-aa99-84645f675b62/workspace/74/spark/notebook/890816',
+                             connection_name='login',
+                             login_password='loginpassword')
+    operator.execute()
