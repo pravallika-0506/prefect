4686223d8ca23564e6e65320178e00c6712d9cde
pravallika-0506 <111057227+pravallika-0506@users.noreply.github.com>
2024-06-18 20:52:58 +0530

    Create sample_notebook.py

---
+++ code/sample_notebook.py
@@ -0,0 +1,11 @@
+from prefect import flow, task
+from prefect.blocks.system import Secret
+from operators.yeedu import YeeduOperator
+
+
+@flow(retries=0, retry_delay_seconds=5, log_prints=True)
+def notebook_run_30_hours():
+    operator = YeeduOperator(job_url='https://dev-onprem-002.yeedu.io/tenant/d3f52457-ba55-4a97-b067-d8f72fb6f9be/workspace/1/spark/notebook/1',
+                             connection_name='connectionldaplocal',
+                             login_password='password-ldap')
+    operator.execute()
