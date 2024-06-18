04400c7fb8e61eefaea3441fcb74ced2078d0173
pravallika-0506 <111057227+pravallika-0506@users.noreply.github.com>
2024-06-18 18:28:54 +0530

    Update 30_hours_notebook.py

--- code/30_hours_notebook.py
+++ code/30_hours_notebook.py
@@ -5,7 +5,7 @@ from operators.yeedu import YeeduOperator
 
 @flow(retries=0, retry_delay_seconds=5, log_prints=True)
 def notebook_run_30_hours():
-    operator = YeeduOperator(job_url='https://qa-aws-002.yeedu.io/tenant/a6a9c5ea-57b6-4a1c-aa99-84645f675b62/workspace/74/spark/notebook/890826',
+    operator = YeeduOperator(job_url='http://10.50.0.178:5173/tenant/d3f52457-ba55-4a97-b067-d8f72fb6f9be/workspace/1/spark/notebook/1',
                              connection_name='login',
                              login_password='loginpassword')
     operator.execute()
