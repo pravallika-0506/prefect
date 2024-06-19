cf475b9bbf3d1ff76c1874ed6d741cf9aa6ac44d
pravallika-0506 <111057227+pravallika-0506@users.noreply.github.com>
2024-06-19 23:23:41 +0530

    Update and rename 30_hours_notebook_run.py to notebook_run_30_hours.py

--- code/notebook_run_30_hours.py
+++ code/notebook_run_30_hours.py
@@ -3,7 +3,7 @@ from operators.yeedu import YeeduOperator
 
 
 @flow(retries=0, retry_delay_seconds=5, log_prints=True)
-def 30_hours_notebook_run():
+def notebook_run():
     operator = YeeduOperator(job_url='http://dev-onprem-002.yeedu.io/tenant/d3f52457-ba55-4a97-b067-d8f72fb6f9be/workspace/1/spark/notebook/2',
                              connection_name='login-ldap',
                              login_password='password-ldap')
